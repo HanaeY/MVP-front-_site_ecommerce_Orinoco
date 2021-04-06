07a9bc787f21c7d294a696189a32b53d4cf899eb
Hanae <hanae.yamashita@lilo.org>
2021-04-06 17:42:32 +0200

    structure et design panier

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,8 +1,4 @@
-/*window.onload = function() {
-    var titre = document.getElementById('titre');
-    titre.textContent = "coucou";
-}
-*/
+/* afficher les produits sur la page produit */
 
 fetch('http://localhost:3000/api/teddies')
 .then(response => response.json())
@@ -13,6 +9,14 @@ fetch('http://localhost:3000/api/teddies')
         document.getElementById(`price-teddy-${i}`).textContent = teddies[i-1].price /100 + " €";
         document.getElementById(`img-teddy-${i}`).setAttribute("src", teddies[i-1].imageUrl);
         document.getElementById(`img-teddy-${i}`).setAttribute("alt", "photo de " + teddies[i-1].name);
+        document.getElementById(`btn-teddy-${i}`).addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(teddies[i-1]);
+            localStorage.setItem('selectedTeddy', JSON.stringify(teddies[i-1])); 
+        })
     }
 })
+
 .catch(error => console.log("erreur :", error))
+
+/* afficher le produit sélectionné sur la page Produits */
