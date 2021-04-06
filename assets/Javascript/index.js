94e3f5dc50c12abb4a5a2f3d0b66b33eae98151a
Hanae <hanae.yamashita@lilo.org>
2021-04-06 10:32:28 +0200

    ajout des produits à la liste

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -3,3 +3,16 @@
     titre.textContent = "coucou";
 }
 */
+
+fetch('http://localhost:3000/api/teddies')
+.then(response => response.json())
+.then(teddies => {
+    for(let i = 1 ; i < 5 ; i++) {
+        document.getElementById(`name-teddy-${i}`).textContent = teddies[i-1].name;
+        document.getElementById(`description-teddy-${i}`).textContent = teddies[i-1].description;
+        document.getElementById(`price-teddy-${i}`).textContent = teddies[i-1].price /100 + " €";
+        document.getElementById(`img-teddy-${i}`).setAttribute("src", teddies[i-1].imageUrl);
+        document.getElementById(`img-teddy-${i}`).setAttribute("alt", "photo de " + teddies[i-1].name);
+    }
+})
+.catch(error => console.log("erreur :", error))
