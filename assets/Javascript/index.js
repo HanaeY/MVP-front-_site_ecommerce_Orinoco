e161667ebe954b6e208bc6a8fbc09fda1db9701b
Hanae <hanae.yamashita@lilo.org>
2021-04-08 18:26:02 +0200

    Récup de l'ID du produit choisi + diverses modifs

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,20 +1,43 @@
-/* afficher les produits sur la page produit */
-// l'API fetch permet de faire des requêtes ajax sans utliser l'objet xhr
-fetch('http://localhost:3000/api/teddies')
-.then(response => response.json()) // la méthode .json permet de parser la réponse et donne une nouvelle promesse 
-.then(teddies => {
-    for(let i = 1 ; i < 5 ; i++) {
-        document.getElementById(`name-teddy-${i}`).textContent = teddies[i-1].name;
-        document.getElementById(`description-teddy-${i}`).textContent = teddies[i-1].description;
-        document.getElementById(`price-teddy-${i}`).textContent = teddies[i-1].price /100 + " €";
-        document.getElementById(`img-teddy-${i}`).setAttribute("src", teddies[i-1].imageUrl);
-        document.getElementById(`img-teddy-${i}`).setAttribute("alt", "photo de " + teddies[i-1].name);
-        document.getElementById(`btn-teddy-${i}`).addEventListener('click', (e) => {
-            localStorage.setItem('selectedTeddy', JSON.stringify(teddies[i-1])); // stocke les info du produit dans le local storage
-        })
+/* AFFICHER LES TEDDIES SUR LA PAGE INDEX */
+
+// Appel de l'API pour récupérer les données des teddies 
+const getTeddies = async () => {
+    try {
+        var response = await fetch('https://ab-p5-api.herokuapp.com/api/teddies');
+        if(response.ok) {
+            var teddies = await response.json(); // la méthode .json retourne une promesse 
+            displayTeddies(teddies)
+        } else {
+            console.error('retour du serveur : ', response);
+        }
+    } catch(err) {
+        console.log('erreur', err);
     }
-})
+}
 
-.catch(error => console.log("erreur :", error))
+// Modification du DOM pour insérer les données des teddies sur la page 
+const displayTeddies = (data) => {
+    for(let i = 1 ; i < 5 ; i++) {
+        document.getElementById(`name-teddy-${i}`).textContent = data[i-1].name;
+        document.getElementById(`description-teddy-${i}`).textContent = data[i-1].description;
+        document.getElementById(`price-teddy-${i}`).textContent = data[i-1].price /100 + " €";
+        document.getElementById(`img-teddy-${i}`).setAttribute("src", data[i-1].imageUrl);
+        document.getElementById(`img-teddy-${i}`).setAttribute("alt", "photo de " + data[i-1].name);
+        document.getElementById(`btn-teddy-${i}`).setAttribute("href", "product.html?id=" + data[i-1]._id);
+    }
+}
 
+getTeddies()
 
+// TO DO : 
+// faire une recherche sur les paramètres de requête en js >> jeudi
+// page produit appelle les param de la requête 
+// recherche sur les param de la requête >> retrouver l'ID 
+// insérer l'id du produit à l'URL product.html 
+// ajouter quantité à la page produit 
+// implémenter l'ajout de teddy au panier 
+// recherche sur le local storage 
+// passer une commande et récupérer l'identifiant 
+// https://openclassrooms.workplace.com/100049595616417/videos/173124777684042
+
+// prochaine session jeudi 15 à 15h. 
--- assets/Javascript/product.js
+++ assets/Javascript/product.js
@@ -1,1 +1,23 @@
-/* afficher le produit sélectionné sur la page Produits */
+/*const displaySelectedTeddy = (data) => {
+    document.getElementById("selected-teddy-name").textContent = data.name;
+    document.getElementById("selected-teddy-description").textContent = data.description;
+    document.getElementById("selected-teddy-colors").textContent = data.color;
+    document.getElementById("selected-teddy-price").textContent = data.price;
+}
+*/
+
+/* AFFICHER DYNAMIQUEMENT LES DONNÉES D'UN TEDDY SUR LA PAGE PRODUIT */
+
+// Utilisation des query parameters pour récupérer l'ID du teddy sélectionné 
+
+const getTeddyId = () => {
+    console.log(window.location); 
+    const urlParam = window.location.search;
+    console.log(urlParam); 
+    const teddyId = urlParam.replace("?id=", ""); 
+    return teddyId
+};
+
+getTeddyId()
+
+/* Appel de l'API pour récupérer les données du Teddy sélectionné */
