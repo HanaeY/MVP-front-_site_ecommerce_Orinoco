9a4f0d1ba883989d09c0e7c70038ee099a1b4657
Hanae <hanae.yamashita@lilo.org>
2021-04-14 11:52:30 +0200

    afficher résumer commande sur la page panier (ok)

--- assets/Javascript/basket.js
+++ assets/Javascript/basket.js
@@ -21,17 +21,41 @@ const summarizeOrder = () => {
             let currentQty = parseInt(basket.get(newId).quantity); 
             currentQty += qty;
             basket.delete(newId);
-            let newItem = {id: storageContent[i].id, color: storageContent[i].color, quantity: currentQty};
+            let newItem = {id: storageContent[i].id, name: storageContent[i].name, color: storageContent[i].color, quantity: currentQty, price: storageContent[i].price};
             basket.set(newId, newItem);
         } else {
             basket.set(newId, storageContent[i]);
         }
     }
     console.log(basket);
-    console.log('test', basket.get(key[0]).name);
+    //console.log('test', basket.get('5beaa8bf1c9d440000a57d94Palebrown').name); // affiche test Arnold 
+    //console.log('test2', basket.get(key[2]).name); // erreur : key is not defined 
+    //console.log('test3', basket.get('').name); 
 };
 
 // Afficher les données du résumé de commande sur la page 
+
+const displayData = () => {
+    for(let item of basket.values()){
+        console.log(item.name);
+        let newLine = document.createElement('tr');
+        document.getElementById('table').appendChild(newLine);
+        let name = document.createElement('td');
+        let color = document.createElement('td');
+        let quantity = document.createElement('td');
+        let price = document.createElement('td');
+        newLine.appendChild(name);
+        newLine.appendChild(color);
+        newLine.appendChild(quantity);
+        newLine.appendChild(price);
+        name.textContent = item.name;
+        color.textContent = item.color;
+        quantity.textContent = item.quantity;
+        price.textContent = item.price;
+    }
+}
+
+/*
 const displayData = () => {
     for(let i = 0 ; i < basket.length ; i++) {
         let newLine = document.createElement('tr');
@@ -49,9 +73,9 @@ const displayData = () => {
         quantity.textContent = basket.get(key[i]).quantity;
         price.textContent = basket.get(key[i]).price;
     }
-};
+};*/
 
 summarizeOrder()
-//displayData()
+displayData()
 
 
