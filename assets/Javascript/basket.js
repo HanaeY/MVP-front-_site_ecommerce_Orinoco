e21f718d881a15f6e637d33b64a27d20cfae65ae
Hanae <hanae.yamashita@lilo.org>
2021-04-15 18:27:39 +0200

    création de la requête POST

--- assets/Javascript/basket.js
+++ assets/Javascript/basket.js
@@ -2,7 +2,9 @@
 const basket = new Map(); 
 let basketBtn = document.getElementById('clear-basket');
 let productArray = [];
+let contact = {};
 let submitBtn = document.getElementById('submit');
+submitBtn.disabled = true;
 
 class Contact {
     constructor(firstName, lastName, address, city, email) {
@@ -76,7 +78,7 @@ const displayData = (data) => {
         quantity.textContent = item.quantity;
         price.textContent = item.price;
     }
-}
+};
 
 // Afficher le prix total 
 
@@ -91,7 +93,7 @@ const displayTotalPrice = (data) => {
     document.getElementById('price-container').appendChild(displayedPrice);
     displayedPrice.textContent = 'Prix total à payer : ' + totalPrice + ' €';
     displayedPrice.classList.add('text-primary', 'text-center', 'border', 'border-primary');
-}
+};
 
 // Vider le panier 
 
@@ -102,6 +104,7 @@ const clearBasket = () => {
     productArray = [];
     console.log('array quand panier vidé ', productArray);
     basketBtn.disabled = true;
+    submitBtn.disabled = true;
 };
 
 /* Tableau de produits à envoyer au serveur */
@@ -124,54 +127,85 @@ const buildOrderArray = () => {
             }
         });
         console.log('tableau à envoyer ', productArray);
+        submitBtn.disabled = false;
 
     }
-}
+};
+
+/// 
 
 processBasket()
 buildOrderArray()
 
-/* Validation des données du formulaire */
+/* Envoi de la commande */
 
-document.forms['order-form'].addEventListener('submit', (e) => {
-    let error;
-    let inputs = this;
-    let formMessage = document.getElementById('input-alert');
+// Validation des données du formulaire 
 
-    if(!inputs['city'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
-        error = 'veuillez renseigner un nom de ville valide';
-    }
-
-    if(!inputs['address'].value.match(/^[a-zA-Z0-9\s,'-.]*[/]{0,1}$/g)) {
-        error = 'veuillez renseigner une adresse valide';
-    }
+ const checkformInputs = () => {
+    document.forms['order-form'].addEventListener('submit', (e) => {
+        let error;
+        let inputs = this;
+        let formMessage = document.getElementById('input-alert');
     
-    if(inputs['email'].value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) { 
-        error = 'veuillez renseigner un email valide';
-        console.log(inputs['email'].validity);
-    }
+        if(!inputs['city'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
+            error = 'veuillez renseigner un nom de ville valide';
+        }
+    
+        if(!inputs['address'].value.match(/^[a-zA-Z0-9\s,'-.]*[/]{0,1}$/g)) {
+            error = 'veuillez renseigner une adresse valide';
+        }
+        
+        if(inputs['email'].value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) { 
+            error = 'veuillez renseigner un email valide';
+            console.log(inputs['email'].validity);
+        }
+    
+        if(!inputs['firstName'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
+            error = 'veuillez renseigner un prénom valide';
+        }
+    
+        if(!inputs['lastName'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
+            error = 'veuillez renseigner un nom valide';
+        }
+    
+        if(error) {
+            e.preventDefault();
+            formMessage.textContent = error;
+            formMessage.classList.add('alert', 'alert-danger');
+        } else {
+            e.preventDefault();
+            formMessage.textContent = 'commande envoyée';
+            contact = new Contact(inputs['firstName'].value, inputs['lastName'].value, inputs['address'].value, inputs['city'].value, inputs['email'].value);
+            console.log('objet contact', contact);
+            send()
+            
+            
+        }
+    });
+ };
 
-    if(!inputs['firstName'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
-        error = 'veuillez renseigner un prénom valide';
-    }
+ // Envoi au serveur 
 
-    if(!inputs['lastName'].value.match(/[A-Za-z\ë\é\è\ê\ï\à\ù\ç\ü\ä\-]+$/g)) {
-        error = 'veuillez renseigner un nom valide';
+ const send = async () => {
+    try {
+        let response = await fetch('http://localhost:3000/api/teddies/order', {
+            method: 'POST',
+            body: JSON.stringify(contact) + JSON.stringify(productArray),
+        });
+        if(response.ok) {
+            let orderRef = await response.json();
+            console.log('ref de la commande', orderRef);
+        } else {
+            console.log('retour du serveur ', response);
+        }
+    } catch(err) {
+        console.log('erreur ', err);
     }
+ };
 
-    if(error) {
-        e.preventDefault();
-        formMessage.textContent = error;
-        formMessage.classList.add('alert', 'alert-danger');
-    } else {
-        e.preventDefault();
-        formMessage.textContent = 'commande envoyée';
-        let contact = new Contact(inputs['firstName'].value, inputs['lastName'].value, inputs['address'].value, inputs['city'].value, inputs['email'].value);
-        console.log('objet contact', contact);
-    }
-});
+ ///
    
-
+ checkformInputs()
 
 
 
